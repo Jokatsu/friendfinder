19ca36ac4cc4a39edf4c86cdb13cc45f11b4b9b5
Jalen Okutsu <jalenokutsu@gmail.com>
2019-04-29 07:19:28 -0700

    final

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,30 +1,30 @@
 var matches = [
     {
-      name: "Ahmed",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+      name: "James Firaza",
+      photo: "https://i1.rgstatic.net/ii/profile.image/398613009190913-1472048155270_Q512/Paul_Nimrod_Firaza.jpg",
       scores: [
-        "5",
-        "1",
+        "3",
         "4",
-        "4",
-        "5",
-        "1",
         "2",
+        "2",
+        "1",
         "5",
-        "4",
+        "5",
+        "5",
+        "2",
         "1"
       ]
     },
     {
-      name: "Jacob Deming",
-      photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+      name: "Andrew Milton",
+      photo: "https://s3-us-west-1.amazonaws.com/co-directory-images/andrew-milton-aa71a572.jpg",
       scores: [
-        "4",
+        "1",
+        "1",
+        "5",
         "2",
         "5",
-        "1",
-        "3",
-        "2",
+        "5",
         "2",
         "1",
         "3",
@@ -32,70 +32,22 @@ var matches = [
       ]
     },
     {
-      name: "Jeremiah Scanlon",
-      photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+      name: "Jess Amila",
+      photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE1JIIhTCG_x96l0zUnNYCcWP6jATm3fCCJaVDq25J3m88VX8RmA",
       scores: [
         "5",
         "2",
         "2",
-        "2",
-        "4",
-        "1",
         "3",
-        "2",
+        "3",
+        "3",
+        "1",
+        "5",
         "5",
         "5"
       ]
     },
-    {
-      name: "Louis T. Delia",
-      photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
-      scores: [
-        "3",
-        "3",
-        "4",
-        "2",
-        "2",
-        "1",
-        "3",
-        "2",
-        "2",
-        "3"
-      ]
-    },
-    {
-      name: "Lou Ritter",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
-      scores: [
-        "4",
-        "3",
-        "4",
-        "1",
-        "5",
-        "2",
-        "5",
-        "3",
-        "1",
-        "4"
-      ]
-    },
-    {
-      name: "Jordan Biason",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
-      scores: [
-        "4",
-        "4",
-        "2",
-        "3",
-        "2",
-        "2",
-        "3",
-        "2",
-        "4",
-        "5"
-      ]
-    }
-  
+
   ];
   
  
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,5 @@
 var matches = require('../data/friends');
+var path = require("path");
 
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
@@ -6,11 +7,43 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        var userInput = req.body;
-        matches.push(userInput);
 
-        res.json(matches);
+        var input = req.body;
+        var inputScores = input.scores;
+        var totalDiff;
 
+        var matchedFriend = {
+            name: "",
+            photo: "",
+            friendDifference: Infinity
+        };
+
+        for (var i = 0; i < matches.length; i++) {
+            var currentFriend = matches[i];
+            totalDiff = 0;
+
+            for (var j = 0; j < currentFriend.scores.length; j++) {
+                var currentFriendScore = currentFriend.scores[j];
+                var currentUserScore = inputScores[j];
+
+                totalDiff += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+            }
+            if (totalDiff <= matchedFriend.friendDifference) {
+                matchedFriend.name = currentFriend.name;
+                matchedFriend.photo = currentFriend.photo;
+                matchedFriend.friendDifference = totalDiff;
+            }
+        }
+        matches.push(input);
+        console.log(matchedFriend);
+        res.json(matchedFriend);
     })
 
+
+
 }
+
+
+
+
+
